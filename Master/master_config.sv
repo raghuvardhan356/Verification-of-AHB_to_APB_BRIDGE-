e95afdf476074c5a7458731aa73d6dfed5f42e19
raghuvardhan356 <raghuvardhanchowdam@gmail.com>
2025-05-11 11:10:41 +0530

    Update master_config.sv

--- Master/master_config.sv
+++ Master/master_config.sv
@@ -1,1 +1,9 @@
+class master_config extends uvm_object;
+ `uvm_object_utils(master_config)
+  virtual Bridge_intf vif;
+  uvm_active_passive_enum is_active;
+  function new(string name="master_config");
+   super.new(name);
+  endfunction
+endclass
 
